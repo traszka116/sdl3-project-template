051da1bf7630d9946fc9a6f6d7061eff6579bf04
newt116 <traszka116@gmail.com>
2025-11-26 22:39:52 +0100

    got wiser removed SDL_image

--- src/main.c
+++ src/main.c
@@ -1,33 +1,18 @@
 #include "SDL3/SDL.h"
 #include <stdbool.h>
+#include <assert.h>
 
 int main(void)
 {
-    if (!SDL_Init(SDL_INIT_VIDEO))
-    {
-        SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
-        return 1;
-    }
-
+    assert(SDL_Init(SDL_INIT_VIDEO));
     SDL_Window *window = SDL_CreateWindow("SDL3 Window", 800, 600, 0);
-    if (!window)
-    {
-        SDL_Log("Failed to create window: %s", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
+    assert(window);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
-    if (!renderer)
-    {
-        SDL_Log("Failed to create renderer: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
+    assert(renderer);
+    
     bool running = true;
     SDL_Event event;
+
 
     while (running)
     {
